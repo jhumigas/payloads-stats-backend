1dfdf2efd7d1d73ed4f8ea2e8565e71916419f3d
David Mugisha <AL1027015@tdf.hubtotal.net>
2021-09-16 10:34:04 +0200

    feat: add payload aggregation to stats

--- src/main/java/com/company/payloadsstatsbackend/common/Constants.java
+++ src/main/java/com/company/payloadsstatsbackend/common/Constants.java
@@ -2,6 +2,7 @@ package com.company.payloadsstatsbackend.common;
 
 public class Constants {
     public static final int WINDOW_LENGTH_IN_SECONDS = 5 * 60;
+    public static final String ZONE_ID = "Europe/Paris";
 
     private Constants() {
         throw new IllegalStateException("Utility class");
--- src/main/java/com/company/payloadsstatsbackend/controller/StatController.java
+++ src/main/java/com/company/payloadsstatsbackend/controller/StatController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
+import com.company.payloadsstatsbackend.common.Constants;
 import com.company.payloadsstatsbackend.dto.PayloadDto;
 import com.company.payloadsstatsbackend.dto.StatDto;
 import com.company.payloadsstatsbackend.mappers.MapStructMapper;
@@ -39,12 +40,28 @@ public class StatController {
     }
 
     @PostMapping("/stats")
-    public StatDto addPayloadToStat(@RequestBody PayloadDto payload) {
-        LocalDateTime receivedAt = LocalDateTime.now().withMinute(0).withSecond(0).withNano(0);
-        Instant statTime = receivedAt.atZone(ZoneId.of("Europe/Paris")).toInstant();
-        StatDto statDto = initStatFromPayload(payload, statTime);
-        Stat savedStat = statService.saveStat(mapStructMapper.statDtoToStat(statDto));
+    public StatDto addPayloadToStat(@RequestBody PayloadDto payloadDto) {
+        LocalDateTime receivedAt = LocalDateTime.now().withNano(0);
+        Instant statTime = getRelatedTimeWindowHighBound(receivedAt);
+        StatDto matchingStatDto = getStatsByCustomerAndContent(payloadDto.getCustomer(), payloadDto.getContent())
+                .stream().filter(stat -> stat.getTime().equals(statTime)).findAny().orElse(null);
+        if (matchingStatDto != null) {
+            matchingStatDto.setCdn(matchingStatDto.getCdn() + payloadDto.getCdn());
+            matchingStatDto.setP2p(matchingStatDto.getP2p() + payloadDto.getP2p());
+        } else {
+            matchingStatDto = initStatFromPayload(payloadDto, statTime);
+        }
+        Stat savedStat = statService.saveStat(mapStructMapper.statDtoToStat(matchingStatDto));
         return mapStructMapper.statToStatDto(savedStat);
+    }
+
+    public Instant getRelatedTimeWindowHighBound(LocalDateTime inputDateTime) {
+        int inputDateTimeSeconds = inputDateTime.getMinute() * 60 + inputDateTime.getSecond();
+        int timeWindowIndex = inputDateTimeSeconds / Constants.WINDOW_LENGTH_IN_SECONDS;
+        int timeWindowHighBoundSeconds = (1 + timeWindowIndex) * Constants.WINDOW_LENGTH_IN_SECONDS;
+        LocalDateTime timeWindowHighBound = inputDateTime.withMinute(0).withSecond(0)
+                .plusSeconds(timeWindowHighBoundSeconds);
+        return timeWindowHighBound.atZone(ZoneId.of(Constants.ZONE_ID)).toInstant();
     }
 
     public StatDto initStatFromPayload(PayloadDto payload, Instant time) {
